bf60d7eb786811ff2f095bcbba6feb27cf8d1dc5
filmifa <92862714+filmifa@users.noreply.github.com>
2021-10-20 16:48:56 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'nissanabi 11'
+copyright = '2021, nissanabi11'
+author = 'nissanabi11'
 
-release = '0.1'
-version = '0.1.0'
+
 
 # -- General configuration
 
